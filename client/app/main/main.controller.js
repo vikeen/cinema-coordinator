eb23be61613f7bbf1d507d23cddbf4353d5082f9
John Rake <john.rake12@gmail.com>
2016-03-28 09:53:17 -0500

    remove "thing" placeholder from the api

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,22 +9,7 @@ class MainController {
     this.awesomeThings = [];
   }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  $onInit() {}
 }
 
 angular.module('cinemaCoordinatorApp')
